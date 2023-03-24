d419f6a1ac3fcedd018a265a4b4ef58bfde5cb19
Kamil Sikora <avler455@gmail.com>
2023-03-24 21:44:48 +0100

    start-02

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,9 @@
+
+export default function Navbar() {
+    return (
+        <div className="cont-navbar">
+            <img src="src\assets\images\menu.svg" ></img>
+            <p className="navbar-text">scoot</p>
+        </div>
+    )
+}
