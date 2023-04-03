df4f8c8db4b1dafccd15066473ac3b8a56a5dd95
Kamil Sikora <avler455@gmail.com>
2023-04-03 19:35:54 +0200

    fix-04

--- src/Pages/About/Section1.tsx
+++ src/Pages/About/Section1.tsx
@@ -1,4 +1,7 @@
 import photo1 from "../../assets/images/About/section1-photo1.png"
+import photo2 from "../../assets/images/About/section1-photo2.png"
+import arrow from "../..//assets/images/Home/arrowpic.svg"
+import arrow2 from "../../assets/images/About/arrow-left.png"
 
 export default function Section1() {
     return(
@@ -9,16 +12,16 @@ export default function Section1() {
                     <h2 className="cont-section1-about-title">Mobility for the digital era</h2>
                     <p className="cont-section1-about-text">Getting around should be simple (and even fun!) for everyone. We embrace technology to provide low cost, smart access to scooters at your fingertips.</p>
                 </div>
-                <img src="src\assets\images\Home\arrowpic.svg" className="section-home-arrow"/>
+                <img src={arrow} className="section-home-arrow"/>
             </div>
             <div className="cont-section1-about-elm2" >
                 
-                <img src="src\assets\images\About\section1-photo2.png"/>
+                <img src={photo2}/>
                 <div className="cont-section1-about-box">
                     <h2 className="cont-section1-about-title">Better urban living</h2>
                     <p className="cont-section1-about-text">We’re helping connect cities and bring people closer together. Our scooters are also fully-electric and we offset the minimal carbon footprint for each ride.</p>
                 </div>
-                <img src="src\assets\images\About\arrow-left.png" className="section-home-arrow"/>
+                <img src={arrow2} className="section-home-arrow"/>
                 </div>
                 
         </div>
